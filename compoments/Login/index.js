46b81a00d17ff389ab2b42fdbabd866ec650bbeb
Caio Augusto <caioaugustov@gmail.com>
2019-05-17 14:25:40 -0300

    Page Login

---
+++ compoments/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainPage from '../common/MainPage'
+// import Content from './Content'
+
+export default function index() {
+  return (
+    <MainPage page="home">
+      {/* <Content/> */}
+    </MainPage>
+  )
+}
---
+++ pages/login.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Login from '../compoments/Login'
+
+export default function login() {
+  return <Login/>
+}
