047ae203d0e4f0df4f33800931c1c0d74426d1dc
Caio Augusto <caioaugustov@gmail.com>
2019-05-17 11:13:08 -0300

    Add API Cursos

--- ducks/SanarFlixSaga.js
+++ ducks/SanarFlixSaga.js
@@ -6,7 +6,7 @@ import { actionTypes, cursosDataRes } from './SanarFlixActions'
 
 function * loadDataCursos () {
   try {
-    const res = yield fetch(`https://pokeapi.co/api/v2/pokemon/1/`)
+    const res = yield fetch(`https://5b7570f8deca780014ec9f86.mockapi.io/v1/cursos`)
     const data = yield res.json()
     yield put(cursosDataRes(data))
   } catch (err) {
@@ -14,6 +14,8 @@ function * loadDataCursos () {
   }
 }
 
+// https://5b7570f8deca780014ec9f86.mockapi.io/v1/professores
+// https://5b7570f8deca780014ec9f86.mockapi.io/v1/modulos
 function * rootSaga () {
   yield all([
     takeLatest(actionTypes.LOAD_DATA, loadDataCursos)
